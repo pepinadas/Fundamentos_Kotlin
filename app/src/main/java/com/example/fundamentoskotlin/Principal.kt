5e0c375653337ea66beb2c7a890b025e2521aa10
pepinadas <mmmunapregunta@gmail.com>
2021-05-03 22:31:47 -0500

    Primer Comit sección 2

---
+++ app/src/main/java/com/example/fundamentoskotlin/Principal.kt
@@ -0,0 +1,28 @@
+package com.example.fundamentoskotlin
+
+fun main () {
+    //print("Hola Kotlin")
+    newTopic("Hola Kotlin")
+
+    newTopic("Variables y Constantes")
+    //val a = 2
+    //val a = "Hola"
+    val a = true
+    println("a = $a")
+
+    //var b = 2
+    var b: Int
+    b = 5
+    println("b = $b")
+}
+
+fun newTopic(topic: String){
+    //println()
+    //print("==================== ")
+    //print(topic)
+    //print(" ==================== ")
+    //println()
+
+    print("\n==================== $topic ====================\n")
+
+}
